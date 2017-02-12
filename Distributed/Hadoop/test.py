b11c25046578c11facd1292cc3fd2f1a8ee203ac
AkDmOl <akshincevdo@gmail.com>
2017-02-12 23:29:48 +0300

    First works with Hadoop and Hive

--- Distributed/Hadoop/test.py
+++
@@ -1,4 +0,0 @@
-import re
-
-res = re.sub(r"[.,;()!?\xe2]", '', "lskdjfskd, lekrlk (\xe2)- dlkdfsl (,?0)! ;dslf;")
-print (res)
--- Distributed/Hive/dz/example5_copy.sql
+++
@@ -1,7 +0,0 @@
-add jar /opt/cloudera/parcels/CDH/jars/hive-contrib-1.1.0-cdh5.7.0.jar;
-USE adoludenko;
-
-add jar Reverser/jar/Reverser.jar;
-create temporary function reverse as 'com.lab4udf.ReverseUDF';
-
-select reverse // to be continued...
