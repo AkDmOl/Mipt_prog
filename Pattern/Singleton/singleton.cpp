8a3f11f9c37b4a0d70dcb0b7b298241df82cd561
AkDmOl <akshincevdo@gmail.com>
2017-02-20 22:50:18 +0300

    Something works wrong....

--- Pattern/Singleton/singleton.cpp
+++ Pattern/Singleton/singleton.cpp
@@ -2,27 +2,28 @@
 
 class Singleton{
 	public:
-		static Singleton* init(){
-			if (Singleton::_instance != NULL)
-				return Singleton::_instance;
+		static Singleton init(){
+			if (Singleton::_instance != NULL){
+				std::cout << "second\n";
+				return *Singleton::_instance;
+			}
 			else{
-				//I should create Singleton, but.... Singleton() in private....
-			}	
-		}
-		void life(){
-			std::cout<<"Hi";
+				std::cout << "first\n";
+				return Singleton();
+			};	
 		}
 	private:
 		Singleton();
 		static Singleton* _instance;
 };
 
+inline Singleton::Singleton() { Singleton::_instance = this; };
 Singleton* Singleton::_instance = NULL;
 
 int main(){
-	Singleton* attempt = Singleton::init();
-	attempt->life();
-	Singleton* attempt_2 = Singleton::init();
-	std::cout << attempt << "	" << attempt_2 << "\n";
+	Singleton attempt = Singleton::init();
+	Singleton attempt_2 = Singleton::init();
+	Singleton attempt_3 = Singleton::init();
+	std::cout << &attempt << "	" << &attempt_2 <<"	"<< &attempt_3 << "\n";
 	return 0;
 }
