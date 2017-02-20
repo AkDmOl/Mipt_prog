e4ecb6d7322a91270d6a4028ec54a88f61837e15
AkDmOl <akshincevdo@gmail.com>
2017-02-20 22:15:25 +0300

    It isn't works...

--- Pattern/Singleton/singleton.cpp
+++ Pattern/Singleton/singleton.cpp
@@ -3,11 +3,11 @@
 class Singleton{
 	public:
 		static Singleton* init(){
-			if (!Singleton::_instance)
+			if (Singleton::_instance != NULL)
 				return Singleton::_instance;
 			else{
-				// ???
-				
+				Singleton::_instance = new Singleton();
+				return Singleton::_instance;
 			}	
 		}
 		void life(){
@@ -23,5 +23,7 @@ Singleton* Singleton::_instance = NULL;
 int main(){
 	Singleton* attempt = Singleton::init();
 	attempt->life();
+	Singleton* attempt_2 = Singleton::init();
+	std::cout << attempt << "	" << attempt_2 << "\n";
 	return 0;
 }
